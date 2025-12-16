474a4b4ca06ae3ee47a234c39a863f83c06aaf5e
Daniel He <me@daniel-egg.com>
2025-12-15 23:15:56 -0800

    Add basic.rectangle_ring

--- gfelib/basic/__init__.py
+++ gfelib/basic/__init__.py
@@ -2,5 +2,6 @@ from __future__ import annotations
 
 from gfelib.basic.chip_border import chip_border
 from gfelib.basic.circle import circle
+from gfelib.basic.rectangle_ring import rectangle_ring
 from gfelib.basic.rectangle import rectangle
 from gfelib.basic.ring import ring
