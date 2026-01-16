b3bb86c86df2883c67e5ac0040b17164dffeb8ae
Yuan Cao <caoyuan96421@gmail.com>
2026-01-15 19:26:45 -0800

    Added Comb and Annular_comb (#15)
    * Added angular comb
    
    * Added linear comb drive/sensor
    
    * Fix docstring on comb and acomb

---
+++ gfelib/actuator/comb.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+
+import numpy as np
+from collections.abc import Sequence
+
+import gfelib as gl
+
+import gdsfactory as gf
+
+
+@gl.utils.default_cell
+def comb(
+    comb_height: float,
+    comb_width: float,
+    comb_gap: float,
+    comb_count: int,
+    comb_overlap: float,
+    geometry_layer: gf.typings.LayerSpec,
+) -> gf.Component:
+    """
+    Generates a comb structure composed of fingers.
+
+    Parameters
+    ----------
+    comb_height : float
+        Total height of the comb
+    comb_width : float
+        Width of each finger
+    comb_gap : float
+        x gap between adjacent fingers.
+    comb_count : int
+        Number of gaps (number of fingers = comb_count + 1).
+    comb_overlap : float
+        x overlap between the fingers
+    geometry_layer : LayerSpec
+        GDS layer for geometry.
+
+    Returns
+    -------
+    gf.Component
+    """
+    c = gf.Component()
+
+    # Number of fingers
+    n_fingers = comb_count + 1
+    comb_length = (comb_height + comb_overlap) / 2
+    comb_clearance = comb_height - comb_length
+
+    for i in range(n_fingers):
+        x = i * (comb_width + comb_gap)
+        y = 0 if (i % 2 == 0) else comb_clearance
+
+        finger = gf.components.rectangle(
+            size=(comb_width, comb_length), layer=geometry_layer, centered=False
+        )
+
+        (c << finger).move((x, y))
+
+    return c
